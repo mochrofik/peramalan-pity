6148d7cd31f66fb1121d0d05394b333a98ee31de
Fitry <zaynfitry@gmail.com>
2023-12-16 21:28:28 +0700

    revisi judul dan hapus file

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -7,7 +7,8 @@
             <div class="card-body">
                 <div class="text-center">
                     <h4>Sistem Peramalan Produksi Perkebunan Besar</h4>
-                    <h4>Menggunakan Perbandingan Metode Holt's ES</h4>
+                    <h4>Menurut Jenis Tanaman</h4>
+                    <h4>Menggunakan Metode Holt's ES</h4>
                     <h4>Dan Metode Winter's ES</h4>
                     <div class="div" style="height: 300px"></div>
                 </div>
